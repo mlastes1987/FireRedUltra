87f186d874ebb64cea30c4b01918026c9c3fda63
MARK\mlast <mlastes1987@gmail.com>
2025-10-04 16:30:58 -0400

    Merge with Cawtds upstream.  Returned starters to originals and edited README.md to reflect this change.  Kadabra learns Shock Wave at lvl 18 and Alakazam learns Thunderbolt at lvl 38.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1228,11 +1228,11 @@
 #define FLAG_HIDDEN_ITEM_SSANNE_EXTERIOR_LAVA_COOKIE                       (FLAG_HIDDEN_ITEMS_START + 190)
 
 // Unused hidden item flags
-#define FLAG_UNUSED_0x4A7               0x4A7 // Unused Flag
+#define FLAG_UNUSED_0x4A7               0x4A7 // Gen 6 Exp Share
 #define FLAG_UNUSED_0x4A8               0x4A8 // Unused Flag
 #define FLAG_UNUSED_0x4A9               0x4A9 // Unused Flag
 #define FLAG_UNUSED_0x4AA               0x4AA // Unused Flag
-#define FLAG_UNUSED_0x4AB               0x4AB // Unused Flag
+#define FLAG_UNUSED_0x4AB               0x4AB // Pokerider
 #define FLAG_UNUSED_0x4AC               0x4AC // Unused Flag
 #define FLAG_UNUSED_0x4AD               0x4AD // Unused Flag
 #define FLAG_UNUSED_0x4AE               0x4AE // Unused Flag
